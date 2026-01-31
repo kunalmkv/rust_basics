7c790cb9d6cce1e8478afe68b28b31caa6723232
kunalmkv <kunalm4.kvtagore@gmail.com>
2026-01-31 14:42:07 +0530

    borrow

--- borrow/target/debug/borrow.d
+++
@@ -1,1 +0,0 @@
-/Users/rajeev/Desktop/rust_basics/course/borrow/target/debug/borrow: /Users/rajeev/Desktop/rust_basics/course/borrow/src/main.rs
--- borrow/target/debug/deps/borrow-de57ac647f5c9bd5.d
+++
@@ -1,5 +0,0 @@
-/Users/rajeev/Desktop/rust_basics/course/borrow/target/debug/deps/borrow-de57ac647f5c9bd5.d: src/main.rs
-
-/Users/rajeev/Desktop/rust_basics/course/borrow/target/debug/deps/borrow-de57ac647f5c9bd5: src/main.rs
-
-src/main.rs:
