e9e10ee3d29575e84268ab79901e1cb41b4cb8d5
kunalmkv <kunalm4.kvtagore@gmail.com>
2026-02-18 13:50:26 +0530

    Add Shapes2 enum with methods for shape declaration in main.rs

--- enum_learn/src/main.rs
+++ enum_learn/src/main.rs
@@ -14,3 +14,30 @@ fn main() {
     let circle: Shape = Shape::new_circle();
     println!("Circle:{:?}", circle);
 }
+
+
+
+enum Shapes2 {
+    Circle(f64),
+    Rectangle(f64, f64),
+    Triangle(f64, f64, f64),
+}
+ 
+impl Shapes2 {
+
+    fn declare_circle(radius: f64) -> Self {
+        Self::Circle(radius)
+    }
+
+    fn declare_rectangle(length: f64, width: f64) -> Self {
+        Self::Rectangle(length, width)
+    }
+
+    fn declare_triangle(a: f64, b: f64, c: f64) -> Self {
+        Self::Triangle(a, b, c)
+    }
+}
+
+fn main() {
+    let circle = Shapes2::declare_circle(10.0);
+}
